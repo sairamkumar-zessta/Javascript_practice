a7f267d69a82f7bc3dde38e89b4ce203b03ed376
admin <sairamkumar.alladi@zessta.com>
2023-04-23 15:39:00 +0530

    todo

---
+++ 7.todo_list1/todo.js
@@ -0,0 +1,46 @@
+/* Follow the instructions found in the description to complete the JavaScript functionality.*/
+const todoItemsContainer = document.getElementById("todoItemsContainer");
+const todoUserInput = document.getElementById("todoUserInput");
+const addBtn = document.getElementById("addBtn");
+let todoList = [
+  {
+    text: "Learn HTML"
+  },
+  {
+    text: "Learn CSS"
+  },
+  {
+    text: "Learn JavaScript"
+  }
+];
+const addAndAppendTodo = (todo)=>{
+    const todoListEl = document.createElement('li');
+    todoItemsContainer.appendChild(todoListEl);
+    todoItemsContainer.style.listStyleType ="none";
+    todoListEl.classList.add("d-flex","todo-item-container");
+    const checkboxInput = document.createElement('input');
+    checkboxInput.type = "checkbox";
+    checkboxInput.classList.add("checkbox-input");
+    todoListEl.appendChild(checkboxInput);
+    const labelCont = document.createElement("div");
+    labelCont.classList.add("label-container","d-flex");
+    todoListEl.appendChild(labelCont);
+    const todoTaskLabel = document.createElement("label");
+    todoTaskLabel.classList.add("checkbox-label");
+    todoTaskLabel.textContent= todo;
+    labelCont.appendChild(todoTaskLabel);
+    const deleteIcon = document.createElement("i");
+    deleteIcon.classList.add("fa-solid","fa-trash","delete-icon-container","delete-icon");
+    labelCont.appendChild(deleteIcon);
+}
+addBtn.onclick=()=>{
+    if (todoUserInput.value.length === 0){
+        alert("Please enter valid Input");
+    }
+    else{
+        addAndAppendTodo(todoUserInput.value);
+    }
+}
+for (const each of todoList){
+    addAndAppendTodo(each.text);
+}
